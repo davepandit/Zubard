e2809d2c6b373165fbf8917c22dc4fe5f0b62201
Debajyoti Pandit <codeninja0812@gmail.com>
2024-02-23 11:39:29 +0530

    File and folder structure updated

---
+++ src/pages/openAi/OpenAi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OpenAi = () => {
+  return (
+    <div>OpenAi</div>
+  )
+}
+
+export default OpenAi
---
+++ src/pages/zubard/Zubard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Zubard = () => {
+  return (
+    <div>Zubard</div>
+  )
+}
+
+export default Zubard
